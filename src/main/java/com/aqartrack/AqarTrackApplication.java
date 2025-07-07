939cca89fe2399b3619ae57d2838db7e2d2403b9
Naoufal Hdr <naoufalhadra96@gmail.com>
2025-07-07 11:34:37 +0100

    initial commit

---
+++ src/main/java/com/aqartrack/AqarTrackApplication.java
@@ -0,0 +1,13 @@
+package com.aqartrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqarTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqarTrackApplication.class, args);
+	}
+
+}
